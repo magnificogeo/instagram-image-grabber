b3e90195b975c46edd2e5597a7f3507ea706e2c1
george-moh-liquor <moh@liquor.com>
2013-05-19 15:44:45 -0700

    added google search box. search not working properly yet.

--- public/gmaps.js
+++ public/gmaps.js
@@ -1,3 +1,5 @@
+google.maps.event.addDomListener(window, 'load', initialize);
+
 function initialize() {
 
   var mapOptions = {
@@ -6,80 +8,98 @@ function initialize() {
       mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
+  var input = /** @type {HTMLInputElement} */(document.getElementById('target'));
   var map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
+  var markers = [];
+  test = markers;
 
+  var searchBox = new google.maps.places.SearchBox(input);
 
-  marker = new google.maps.Marker({
-    map:map,
-    draggable:true,
-    icon : new google.maps.MarkerImage("../img/black.png", null, null, new google.maps.Point(10,10)),
-    animation: google.maps.Animation.DROP,
-    position: new google.maps.LatLng(37.775,-122.419)
-  });
+  google.maps.event.addListener(searchBox, 'places_changed', function() {
+    var places = searchBox.getPlaces();
 
-  //dragend event executes after the marker is dropped and released
-  google.maps.event.addListener(marker, 'dragend', function() {
+    searchedlat = floorFigure(places[0].geometry.location.jb, 3);
+    searchedlng = floorFigure(places[0].geometry.location.kb, 3);
+
+    var newmapOptions = {
+
+      center : new google.maps.LatLng(searchedlat,searchedlng),
+      zoom:14,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
     
-    //console.log(marker.getPosition());
-    lat = floorFigure(marker.getPosition().lat(), 3); //converting lat to 3 decimal places
-    lng = floorFigure(marker.getPosition().lng(), 3); //converring lng to 3 decimal places
+    }
+    var newmap = new google.maps.Map(document.getElementById("map-canvas"), newmapOptions);
 
-    console.log(lat);
-    console.log(lng);
-    $.ajax({
-      type : "GET",
-      url: 'https://api.instagram.com/v1/locations/search?lat=' + lat + '&lng=' + lng + '&access_token=31376751.1fb234f.6b202a4372be40be85a6caeda9096adc&callback=?',
-      data : '',
-      dataType : 'jsonp',
-      success: function( location_response ) {
-
-        robject1 = location_response;
-        
-        $('#myModal').modal('toggle');
-        //console.log("success 1!");
-
-        for (counter = 0; counter <= robject1.data.length; counter++) {
-          locationid1 = robject1.data[counter].id;
-          $.ajax({
-            type : "GET",
-            url: 'https://api.instagram.com/v1/locations/' + locationid1 + '/media/recent?access_token=31376751.1fb234f.6b202a4372be40be85a6caeda9096adc&callback=?',
-            data: '',
-            dataType : 'jsonp',
-            success: function( response ) {
-
-            robject2 = response;
-
-            //console.log("success 2!");
-     
-            /*
-            var MyInfoWindow = new google.maps.InfoWindow({ 
-              content: get_marker_content()
-            });
-
-            MyInfoWindow.open(map, marker); 
-            */
-            console.log(robject2);
-
-            render_data(robject2);
-            
-            //close infowindow to refresh DOM ondrag again to a new position
-             google.maps.event.addListener(marker, 'dragend', function() { 
-            //MyInfoWindow.close(); 
-              $('body').html("");
-            } );
-            
-
-            }
-          });
-        }
-        
-      }
+    marker = new google.maps.Marker({
+      map: newmap,
+      draggable:true,
+      icon : new google.maps.MarkerImage("../img/black.png", null, null, new google.maps.Point(10,10)),
+      animation: google.maps.Animation.DROP,
+      position: new google.maps.LatLng(searchedlat,searchedlng),
     });
+
+    mapOptions['center'] = new google.maps.LatLng(searchedlat,searchedlng);
+    markers.push(marker);
+    google.maps.event.addListener(marker, 'idle', begin_ajax_call(marker));
+  
   });
+
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+function begin_ajax_call(marker, searchedlat, searchedlng) {
+    //dragend event executes after the marker is dropped and released
+
+      lat = floorFigure(marker.getPosition().lat(), 3); //converting lat to 3 decimal places
+      lng = floorFigure(marker.getPosition().lng(), 3); //converring lng to 3 decimal places
+
+
+      console.log(lat);
+      console.log(lng);
+      $.ajax({
+        type : "GET",
+        url: 'https://api.instagram.com/v1/locations/search?lat=' + lat + '&lng=' + lng + '&access_token=31376751.1fb234f.6b202a4372be40be85a6caeda9096adc&callback=?',
+        data : '',
+        dataType : 'jsonp',
+        success: function( location_response ) {
+
+          robject1 = location_response;
+          
+          $('#myModal').modal('toggle');
+          //console.log("success 1!");
+
+          for (counter = 0; counter <= robject1.data.length; counter++) {
+            locationid1 = robject1.data[counter].id;
+            $.ajax({
+              type : "GET",
+              url: 'https://api.instagram.com/v1/locations/' + locationid1 + '/media/recent?access_token=31376751.1fb234f.6b202a4372be40be85a6caeda9096adc&callback=?',
+              data: '',
+              dataType : 'jsonp',
+              success: function( response ) {
+
+              robject2 = response;
+
+              console.log(robject2);
+
+              render_data(robject2);
+              
+              //close infowindow to refresh DOM ondrag again to a new position
+               google.maps.event.addListener(marker, 'dragend', function() { 
+              //MyInfoWindow.close(); 
+                $('#infobox').html("");
+              } );
+              
+
+              }
+            });
+          }
+          
+        }
+      });
+  }  
+
+
+
 
 function floorFigure(figure, decimals){
     if (!decimals) decimals = 2;
